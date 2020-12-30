f2dcd115ac97709151815866cd4769e4024f70ff
parkiw <parkiw@woojingns.com>
2020-12-30 10:26:05 +0900

    전문 제외 버튼/상단 제목 정리

--- app/src/main/java/superconn/pds/sw/superconn/junmun/JunmunInformalFragment.java
+++ app/src/main/java/superconn/pds/sw/superconn/junmun/JunmunInformalFragment.java
@@ -22,16 +22,12 @@ import java.util.ArrayList;
 import superconn.pds.sw.superconn.R;
 
 public class JunmunInformalFragment extends Fragment {
-    private Spinner junmun_informal_sp;
-
 
     ArrayList<String> arrayList;
-
 
     public JunmunInformalFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,13 +37,13 @@ public class JunmunInformalFragment extends Fragment {
 
 
         //spinner 장착
-        junmun_informal_sp = view.findViewById(R.id.junmun_informal_sp);
+//        junmun_informal_sp = view.findViewById(R.id.junmun_informal_sp);
 
         arrayList = new ArrayList<>();
-
-        final ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(), R.array.priority, android.R.layout.simple_spinner_dropdown_item);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        junmun_informal_sp.setAdapter(arrayAdapter);
+//
+//        final ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(), R.array.priority, android.R.layout.simple_spinner_dropdown_item);
+//        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        junmun_informal_sp.setAdapter(arrayAdapter);
 
         final EditText junmun_informal_et_content = view.findViewById(R.id.junmun_informal_et_content);
         final TextView junmun_informal_tv_count = view.findViewById(R.id.junmun_informal_tv_count);
@@ -57,20 +53,15 @@ public class JunmunInformalFragment extends Fragment {
         junmun_informal_et_content.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String input = junmun_informal_et_content.getText().toString();
                 junmun_informal_tv_count.setText(input.length()+" / 200 최대 글자 수");
             }
-
             @Override
             public void afterTextChanged(Editable s) {
-
             }
-
         });
 
         //엔터 클릭시 다음줄이 아닌 키보드 안보이게 설정
@@ -83,7 +74,6 @@ public class JunmunInformalFragment extends Fragment {
                     imm.hideSoftInputFromWindow(junmun_informal_et_title.getWindowToken(), 0);    //hide keyboard
                     return true;
                 }
-
                 return false;
             }
         });
@@ -91,13 +81,11 @@ public class JunmunInformalFragment extends Fragment {
         junmun_informal_et_content.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                     InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(junmun_informal_et_content.getWindowToken(), 0);    //hide keyboard
                     return true;
                 }
-
                 return false;
             }
         });
