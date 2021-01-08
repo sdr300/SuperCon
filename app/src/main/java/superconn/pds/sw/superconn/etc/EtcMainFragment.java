366245da3918566fc8550904d24a451fbf2e9384
parkiw <parkiw@woojingns.com>
2021-01-08 15:50:07 +0900

    범용 도식(transparencywrite) 1차완료, 스피너 필요

--- app/src/main/java/superconn/pds/sw/superconn/etc/EtcMainFragment.java
+++ app/src/main/java/superconn/pds/sw/superconn/etc/EtcMainFragment.java
@@ -9,7 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import superconn.pds.sw.superconn.MapActivity;
 import superconn.pds.sw.superconn.R;
--- app/src/main/java/superconn/pds/sw/superconn/etc/TransparencyFragment.java
+++ app/src/main/java/superconn/pds/sw/superconn/etc/TransparencyFragment.java
@@ -8,10 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import superconn.pds.sw.superconn.MapActivity;
 import superconn.pds.sw.superconn.R;
+import superconn.pds.sw.superconn.databinding.FragmentTransparencyBinding;
+import superconn.pds.sw.superconn.junmun.JunmunReceiverDeleteFragment;
 
 
 public class TransparencyFragment extends Fragment {
+
+    private FragmentTransparencyBinding mBinding;
 
     public TransparencyFragment() {
         // Required empty public constructor
@@ -20,7 +25,17 @@ public class TransparencyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transparency, container, false);
+
+        mBinding = FragmentTransparencyBinding.inflate(inflater,container, false);
+
+        mBinding.transparencyBtnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MapActivity.fragmentManager.beginTransaction().replace(R.id.fragment_frame, new TransparencyWriteFragment(), null).addToBackStack(null).commit();
+
+            }
+        });
+
+        return mBinding.getRoot();
     }
 }
